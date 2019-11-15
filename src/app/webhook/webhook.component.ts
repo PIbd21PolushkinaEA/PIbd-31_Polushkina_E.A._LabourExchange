eb578aad1f8b86a23312b300db5b4c3cdaf9d21e
Zhenya <polushkina.zhenya@mail.ru>
2019-11-15 10:57:19 +0300

    Lab_10_WebHook_v.3

--- src/app/webhook/webhook.component.ts
+++ src/app/webhook/webhook.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
+import {VacanciesService} from '../admin/vacancies.service';
 
 @Component({
   selector: 'app-webhook',
@@ -8,14 +9,25 @@ import { Subject } from 'rxjs';
 })
 export class WebhookComponent implements OnInit {
 
-  constructor() {
+  constructor(private vacanciesService: VacanciesService) {
     this.observable_message.subscribe(val => {
       if (this.messages){
         this.messages.push(val);
         console.log(this.messages);
       }
+
+      let message = JSON.parse(val).object.body.split(',');
+        console.log(message);
+        if (message[0] == 'Вакансия'){
+          let d = {id: null, position: message[1], description: message[2], salary: message[3], images: [], images_files: []};
+          this.vacanciesService.addVacancy(d).subscribe(result => {
+            if (result.status == '201') {
+              console.log("Vacancy added successfully: ", result.list);              
+            }
+          });
+        } 
     });
-   }
+  }
 
   messages: string[] = [];
   observable_message = new Subject<string>();
